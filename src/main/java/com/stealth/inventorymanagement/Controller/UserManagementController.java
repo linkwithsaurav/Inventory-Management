9f63cbb28b0c2ef9356c91ea9ca0ff4f85b4219a
Saurav Singh <sauravsingh0721@gmail.com>
2022-07-10 00:25:26 +0530

    initial commit

---
+++ src/main/java/com/stealth/inventorymanagement/Controller/UserManagementController.java
@@ -0,0 +1,9 @@
+package com.stealth.inventorymanagement.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user-management")
+public class UserManagementController {
+
+
+}
